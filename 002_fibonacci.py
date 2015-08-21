e327c176c3283d8f9c062adcea40a68cf50e23e7
FOXintheBOX <thomas.e.turner@gmail.com>
2015-08-21 00:37:22 -0400

    generalised to any divisor

--- 002_fibonacci.py
+++ 002_fibonacci.py
@@ -1,15 +1,15 @@
-def fibonacci(max_value):
+def fibonacci(max_value,divisor):
     vals = [1,2]
     i = 0
     seq = 0
-    sum_even = 0
+    sum_chosen = 0
     while vals[i] < max_value:
-        if vals[i] % 2 == 0:
-            sum_even += vals[i]
+        if vals[i] % divisor == 0:
+            sum_chosen += vals[i]
             print vals[i]
         vals[i] = sum(vals)
         i = 1 - i
     print "...."
-    return sum_even
+    return sum_chosen
 
 print fibonacci(4000000)
