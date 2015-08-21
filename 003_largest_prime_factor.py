6e36f1c1f257ea3f8dc16bafae86568d0f9d4370
FOXintheBOX <thomas.e.turner@gmail.com>
2015-08-21 03:25:01 -0400

    problem 3 done

---
+++ 003_largest_prime_factor.py
@@ -0,0 +1,23 @@
+def is_prime(x):
+    if x == 0 or x == 1:
+        return True
+    for i in range(2,int(x**0.5)+1):
+        if x % i == 0:
+            return False
+    return True
+
+def prime_factors(target):
+    primes=[]
+    i = 2
+    while i < target**0.5:
+        if is_prime(i) and target % i == 0:
+            primes.append(i)
+            target /= i
+            print "prime found: " + str(i)
+            i = 2
+        i += 1
+    primes.append(target)
+    return primes
+
+print prime_factors(600851475143)
+print 71 *839 * 1471 * 6857
